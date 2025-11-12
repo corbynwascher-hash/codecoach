a909b087909a70416724bc6bd236b629ac049f1c
corbynwascher-hash <corbyn.wascher@u304.org>
2025-11-12 14:41:47 -0600

    Set up React application with CodeCoach component

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CodeCoach from './CodeCoach.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CodeCoach />
+  </React.StrictMode>,
+)
+
